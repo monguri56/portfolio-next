c4b8486378725757727c885a4995de9764ab1ea0
Oh Jiyeon <ohjiyeon98@naver.com>
2026-01-09 18:34:08 +0900

    fix

---
+++ components/sections/ContactSection.tsx
@@ -0,0 +1,8 @@
+export default function ProjectsPage() {
+  return (
+    <main style={{ padding: 24 }}>
+      <h1 style={{ fontSize: 28, fontWeight: 700 }}>Projects</h1>
+      <p style={{ marginTop: 12 }}>프로젝트 페이지 준비 중입니다.</p>
+    </main>
+  );
+}
